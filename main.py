f70fb1427b1cbce87df9f005309c9afa9403a3cd
unknown <princeprem295@gmail.com>
2026-09-23 21:21:47 +0530

    main

--- main.py
+++ main.py
@@ -1,25 +1,17 @@
-from langchain_openai import ChatOpenAI
-from langchain_core.prompts import PromptTemplate
-from dotenv import load_dotenv
-import os
-load_dotenv()
+import langchain_helper as lch
+import streamlit as st
+ 
 
-def generate_pet_name(animal_type):
-    llm = ChatOpenAI(
-        model_name="openai/gpt-4o-mini",
-        temperature=0.7,
-        api_key=os.getenv("OPENROUTER_API_KEY"),
-        base_url="https://openrouter.ai/api/v1",
-        )
-    prompt_template_name=PromptTemplate(
-        input_variables=["animal_type"],
-        template="I have a {animal_type} pet and I want a cool name for it. Suggest me five names for my pet",
-    )
-    chain = prompt_template_name | llm
-    response = chain.invoke({
-        "animal_type": animal_type
-        })
-    return response.content
+st.title("Pet Name Generator")
 
-if __name__ == "__main__":
-    print(generate_pet_name("horse"))
+animal_type = st.sidebar.selectbox("what is your pet?",('Cat','Dog','Horse','Cow'))
+
+if animal_type == "Cat":
+    pet_color = st.sidebar.selectbox("what is your pet color",max_char=15)
+
+if animal_type == "Dog":
+    pet_color = st.sidebar.selectbox("what is your pet color",max_char=15)
+if animal_type == "Horse":
+    pet_color = st.sidebar.selectbox("what is your pet color",max_char=15)
+if animal_type == "Cow":
+    pet_color = st.sidebar.selectbox("what is your pet color",max_char=15)
