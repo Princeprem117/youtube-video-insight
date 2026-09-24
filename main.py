7dee97894270c8d96e0e38f138d5b0e1cbf22f12
unknown <princeprem295@gmail.com>
2026-09-24 10:53:02 +0530

    main

--- main.py
+++ main.py
@@ -4,14 +4,19 @@ import streamlit as st
 
 st.title("Pet Name Generator")
 
-animal_type = st.sidebar.selectbox("what is your pet?",('Cat','Dog','Horse','Cow'))
+user_animal_type = st.sidebar.selectbox("what is your pet?",('Cat','Dog','Horse','Cow'))
 
-if animal_type == "Cat":
-    pet_color = st.sidebar.selectbox("what is your pet color",max_char=15)
+if user_animal_type == "Cat":
+    pet_color = st.sidebar.text_area(label="what is your pet color",max_chars=15)
 
-if animal_type == "Dog":
-    pet_color = st.sidebar.selectbox("what is your pet color",max_char=15)
-if animal_type == "Horse":
-    pet_color = st.sidebar.selectbox("what is your pet color",max_char=15)
-if animal_type == "Cow":
-    pet_color = st.sidebar.selectbox("what is your pet color",max_char=15)
+if user_animal_type == "Dog":
+    pet_color = st.sidebar.text_area(label="what is your pet color",max_chars=15)
+if user_animal_type == "Horse":
+    pet_color = st.sidebar.text_area(label="what is your pet color",max_chars=15)
+if user_animal_type == "Cow":
+    pet_color = st.sidebar.text_area(label="what is your pet color",max_chars=15)
+
+
+if pet_color:
+    response = lch.generate_pet_name(user_animal_type, pet_color)
+    st.markdown(response)
